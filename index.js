35dede44ec199f4822d42f2cb1ab433e041b0d61
Mohamad Aid Bacuk Zanab <mohammedsharif067@gmail.com>
2023-05-25 14:10:51 +0300

    Done with the Assignment V2

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
-// function getSum(total, num) {
-//     return total + Math.round(num);
-//   }
 
 let totalBatteries = batteryBatches.reduce((acc, num) => acc + num , 0);
 
